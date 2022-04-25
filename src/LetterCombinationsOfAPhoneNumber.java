f7835faf508d98011416a6e92a465dc48c88d9e1
HongruWang-123 <74997670+HongruWang-123@users.noreply.github.com>
2022-04-25 18:44:38 -0400

    finish

---
+++ src/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LetterCombinationsOfAPhoneNumber {
+	public List<String> letterCombinations(String digits) {
+        if(digits.length()==0) return new ArrayList<String>();
+        Map<Character,ArrayList<Character>> ans = new HashMap<>();
+        ans.add('2',new ArrayList<Charracter>(Arrays));
+    }
+}
